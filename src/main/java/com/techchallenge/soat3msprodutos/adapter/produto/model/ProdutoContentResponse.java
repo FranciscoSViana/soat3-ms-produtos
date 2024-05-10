23c4171a77766622399c25142e78c4ee6ccd7351
Francisco Viana <franciscosantos29@gmail.com>
2024-05-10 14:59:10 -0300

    refatorar para os testes e passar no sonarcloud

--- src/main/java/com/techchallenge/soat3msprodutos/adapter/produto/model/ProdutoContentResponse.java
+++ src/main/java/com/techchallenge/soat3msprodutos/adapter/produto/model/ProdutoContentResponse.java
@@ -3,7 +3,6 @@ package com.techchallenge.soat3msprodutos.adapter.produto.model;
 import lombok.*;
 
 import java.util.List;
-import java.util.Objects;
 
 @Getter
 @Setter
@@ -11,13 +10,6 @@ import java.util.Objects;
 @AllArgsConstructor
 @NoArgsConstructor(force = true)
 public class ProdutoContentResponse {
+    
     private List<ProdutoResponse> content;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ProdutoContentResponse that = (ProdutoContentResponse) o;
-        return Objects.equals(content, that.content);
-    }
 }
